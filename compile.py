de661ee7e55b3427620f470bd5362e335b71a0f7
Jachdich <jamesedkitching@gmail.com>
2022-11-02 16:37:18 +0000

    syntax ideas

--- compile.py
+++ compile.py
@@ -4,23 +4,40 @@ import os
 # expr = Forward()
 
 integer = Combine(Optional("-") + Word("0123456789"))
+decimal = Combine(Optional("-") + Word("0123456789") + Literal(".") + Word("0123456789"))
+number = decimal | integer 
 
-expr = integer
+expr = Forward()
+mulexpr = Forward()
+term = Group("(" + expr + ")") | number
+
+mulexpr << (Group(term + (Literal("*") | Literal("/")) + mulexpr) | term)
+expr << (Group(mulexpr + (Literal("+") | Literal("-")) + expr) | mulexpr)
 
 prog = ZeroOrMore(expr)
 
-for fname in [n for n in os.listdir("tests") if not "_output" in n]:
-    fname = os.path.join("tests", fname)
-    with open(fname + "_output", "r") as f:
-        expected_output = f.read().strip()
-        
-    with open(fname, "r") as f:
-        inp = f.read()
-        
-    out = prog.parseString(inp)
-    if str(out) == expected_output:
-        print("Test " + fname + " passed")
+
+def test(inp, out):
+    try:
+        res = prog.parseString(inp, parseAll=True)
+    except ParseException as e:
+        res = str(e)
+
+    if res == out:
+        print("\x1b[92m✓\x1b[0m", inp, "=>", res)
     else:
-        print("Test " + fname + " FAILED")
-        print("Output: " + str(out))
-        print("Expected output: " + expected_output)
+        print("\x1b[91mx\x1b[0m", inp, "=>", res, "!=", out, "(expected)")
+
+
+def run_tests():
+    test("0", ["0"])
+    test("76139", ["76139"])
+    test("32.33", ["32.33"])
+    test("-341", ["-341"])
+    test("-32.133", ["-32.133"])
+    test("132 + 1.324", ["132", "+", "1.324"])
+    test("1 + 2 + 3", ["1", "+", "2", "+", "3"])
+    test("1 + 2 * 3", ["1", "+", ["2", "*", "3"]])
+    test("1 * 2 + 3", [["1", "*", "2"], "+", "3"])
+
+run_tests()
