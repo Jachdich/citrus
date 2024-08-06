c58bad4345af8b2f9d9e8f37e648aafebc663f51
Jachdich <jek@cospox.com>
2024-08-06 21:23:15 +0100

    shitty janky as fuck c generation

---
+++ citrus.h
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stdio.h>
+#define i8  int8_t
+#define i16 int16_t
+#define i32 int32_t
+#define i64 int64_t
+#define u8  uint8_t
+#define u16 uint16_t
+#define u32 uint32_t
+#define u64 uint64_t
+#define f32 float
+#define f64 double
--- parser.py
+++ parser.py
@@ -19,7 +19,19 @@ class Ast:
         s = "\n".join(self.src.split("\n")[self.lineno-3:self.lineno]) + "\n"
         s += " " * (self.column) + "^ " + str(self.lineno) + ":" + str(self.column)
         return "\n" + s + "\n"
-                
+
+class CG:
+    def __init__(self):
+        self.globals = {}
+    def compile(self, parsed: list[Ast]):
+        total_code = '#include "citrus.h"\n'
+        for smt in parsed:
+            total_code += "// " + str(smt) + "\n"
+            code, exprcode = smt.compile(self, 0)
+            assert exprcode is None, "statements shouldn't generate an expression"
+            total_code += code + "\n\n"
+        return total_code
+
 def type_assert(var, types):
     if not isinstance(var, types):
         raise TypeError(f"Expected type {types} and instead got {type(var)}")
@@ -41,6 +53,9 @@ class NumLit(Ast):
 
     def get_type(self):
         return Type("u16")
+
+    def compile(self, state, indent):
+        return None, str(self.val)
         
 
 class StrLit(Ast):
@@ -58,6 +73,9 @@ class StrLit(Ast):
     def __str__(self):
         return str(self.val)
 
+    def compile(self, state, indent):
+        return None, f'"{self.val}"'
+
 class Ident(Ast):
     name: str
     def __init__(self, src, pos, tokens):
@@ -67,6 +85,9 @@ class Ident(Ast):
 
     def __repr__(self):
         return f"Ident({self.name})"
+
+    def compile(self, state, indent):
+        return None, self.name
 
 class GenericIdent(Ast):
     name: str
@@ -82,6 +103,9 @@ class GenericIdent(Ast):
         gens = f"<{', '.join(map(repr, self.generics))}>" if len(self.generics) > 0 else ""
         return f"GenericIdent({self.name}{gens})"
 
+    def c_name(self) -> str:
+        return self.name
+
 class NamespacedIdent(Ast):
     vals: list[Ident]
     def __init__(self, src, pos, tokens):
@@ -95,14 +119,19 @@ class NamespacedIdent(Ast):
 
 class TypeIdent(Ast):
     vals: list[GenericIdent]
+    num_ptr: int
     def __init__(self, src, pos, tokens):
         super().__init__(src, pos, tokens)
-        self.vals = tokens
+        self.vals = list(tokens[1])
+        self.num_ptr = len(tokens[0])
         [type_assert(g, GenericIdent) for g in self.vals]
         assert len(self.vals) > 0
 
     def __repr__(self):
         return f"TypeIdent({', '.join(map(repr, self.vals))})"
+
+    def c_name(self) -> str:
+        return "_".join([val.c_name() for val in self.vals]) + "*" * self.num_ptr
 
 class BinOp(Ast):
     op: str
@@ -121,6 +150,24 @@ class BinOp(Ast):
     def __str__(self):
         return f" {self.op} ".join(map(str, self.operands))
 
+    def compile(self, state, indent): 
+        return None, f" {self.op} ".join(map(str, self.operands))
+
+class BlockExpr(Ast):
+    smts: list[Ast]
+    expr: Ast
+    def __init__(self, src, pos, tokens):
+        super().__init__(src, pos, tokens)
+        self.smts = tokens[:-1]
+        self.expr = tokens[-1]
+        type_assert(self.smts, list)
+        [type_assert(s, Ast) for s in self.smts]
+        type_assert(self.expr, Ast)
+
+    def __repr__(self):
+        return f"{{ {'; '.join(map(repr, self.smts))}; {self.expr} }}"
+    
+
 class UnOp(Ast):
     op: str
     operand: Ast
@@ -137,27 +184,121 @@ class UnOp(Ast):
     def __str__(self):
         return f"{self.operand}{self.op}"
 
-class FuncExpr(Ast):
+class FuncCall(Ast):
     def __init__(self, src, pos, tokens):
         super().__init__(src, pos, tokens)
-        
+        self.fn_expr = tokens[0]
+        if len(tokens) > 1:
+            self.args = tokens[1:]
+        else:
+            self.args = []
+            
+    def __repr__(self):
+        return f"FuncCall({self.fn_expr}({self.args}))"
+
+    def __str__(self):
+        return f"{self.fn_expr}({self.args})"
+
+    def compile(self, state, indent):
+        fn_expr_body, fn_expr_ret = self.fn_expr.compile(state, indent)
+        args_body, args_ret = zip(*[arg.compile(state, indent) for arg in self.args])
+        bodies = list(filter(lambda x: x is not None, args_body + (fn_expr_body,)))
+        if len(bodies) > 0:
+            body = "\n".join(bodies)
+        else:
+            body = None
+
+        return body, f"{fn_expr_ret}({', '.join(args_ret)})"
+
+class FuncExpr(Ast):
+    generic_defs: list[Ident]
+    args: list[tuple[Ident, TypeIdent]]
+    body: Ast
+    ret_type: TypeIdent | None
+    is_proc: bool
+    def __init__(self, src, pos, tokens):
+        super().__init__(src, pos, tokens)
+        self.is_proc = tokens[0] == "proc"
+        self.generic_defs = list(tokens[1])
+        self.args = [(x[0], x[1]) for x in tokens[2]]
+        self.ret_type = tokens[3]
+        self.body = tokens[4]
+
+        type_assert(self.generic_defs, list)
+        [type_assert(g, Ident) for g in self.generic_defs]
+        type_assert(self.args, list)
+        [(type_assert(g, tuple), type_assert(g[0], Ident), type_assert(g[1], TypeIdent)) for g in self.args]
+        type_assert(self.body, Ast)
+        type_assert(self.ret_type, (TypeIdent, type(None)))
+
+    def __repr__(self):
+        if len(self.generic_defs) > 0:
+            generics = "<" + ", ".join(map(str, self.generic_defs)) + ">"
+        else:
+            generics = ""
+
+        return f"{'proc' if self.is_proc else 'func'}{generics}({', '.join(map(repr, self.args))}): {self.ret_type}, {self.body}"
+
+class Definition(Ast):
+    name: TypeIdent
+    value: Ast
+    def __init__(self, src, pos, tokens):
+        super().__init__(src, pos, tokens)
+        self.name = tokens[1]
+        self.value = tokens[2]
+        type_assert(self.name, TypeIdent)
+        type_assert(self.value, Ast)
+
+    def __repr__(self):
+        return f"const {self.name} = {self.value};"
+
+    def compile(self, state: CG, indent: int):
+        # if defining a function, do it properly
+        if isinstance(self.value, FuncExpr):
+            indent += 1
+            name = self.name.c_name()
+            ret_ty = self.value.ret_type.c_name()
+            args = ", ".join(f"{ty.c_name()} {name.name}" for name, ty in self.value.args)
+            body = ""
+            if isinstance(self.value.body, BlockExpr):
+                for s in self.value.body.smts:
+                    s_body, s_ret = s.compile(state, indent)
+                    if s_body is not None:
+                        body += s_body
+                    if s_ret is not None:
+                        body += f"{' ' * 4 * indent}{s_ret};\n"
+                extra_body, ret = self.value.body.expr.compile(state, indent)
+                if extra_body is not None:
+                    body += extra_body + ";\n"
+            else:
+                body, ret = self.value.body.compile(state, indent)
+            return f"{ret_ty} {name}({args}) {{\n{body}{' ' * 4 * indent}return {ret};\n}}", None
+
+# const add<T> = func<T>(a: T, b: T): T a + b;
 
 ident = pp.Word(alphas + "_", alphanums + "_").set_parse_action(Ident)
 namespaced_ident = pp.DelimitedList(ident, "::").set_parse_action(NamespacedIdent)
 str_lit = (pp.Suppress('"') - pp.Word(pp.printables + " ", exclude_chars='"') - pp.Suppress('"')).set_parse_action(StrLit)
 literal = str_lit | pp.pyparsing_common.number.set_parse_action(NumLit)
 
+expr = pp.Forward()
+
 type_ident = pp.Forward()
 generic_list = pp.DelimitedList(type_ident, ",", allow_trailing_delim=True)
 generic_def_list = pp.Group(pp.DelimitedList(ident, ",", allow_trailing_delim=True))
 generic_ident = (ident - pp.Optional(pp.Suppress("<") - generic_list - pp.Suppress(">"))).set_parse_action(GenericIdent)
-type_ident << pp.DelimitedList(generic_ident, "::").set_parse_action(TypeIdent)
-arg_def = (ident - pp.Suppress(":") - type_ident)
+type_ident << (pp.Group(pp.ZeroOrMore("*")) - pp.Group(pp.DelimitedList(generic_ident, "::"))).set_parse_action(TypeIdent)
+arg_def = pp.Group(ident - pp.Suppress(":") - type_ident)
 arg_def_list = pp.Group(pp.DelimitedList(arg_def, ",", allow_trailing_delim=True))
+arg_expr_list = pp.DelimitedList(expr, ',', allow_trailing_delim=True)
 
-expr = pp.Forward()
+primary_expr = ident | literal | (pp.Suppress("(") + expr + pp.Suppress(")"))
 
-math_expr = pp.infix_notation(expr,
+postfix_expr = pp.Forward()
+postfix_expr <<= (postfix_expr + pp.Suppress("(") + pp.Optional(arg_expr_list) + pp.Suppress(")")).set_parse_action(FuncCall)\
+                 | primary_expr
+
+math_expr = pp.infix_notation(postfix_expr,
     [
         (pp.oneOf("@ &"), 1, pp.opAssoc.RIGHT, UnOp),
         (pp.oneOf("* / %"), 2, pp.opAssoc.LEFT, BinOp),
@@ -168,26 +309,19 @@ math_expr = pp.infix_notation(expr,
 )
 
 func_expr = ((pp.Keyword("func") | pp.Keyword("proc")) -\
-    pp.Optional(pp.Suppress("<") - generic_def_list - pp.Suppress(">")) -\
+    pp.Optional(pp.Suppress("<") - generic_def_list - pp.Suppress(">"), []) -\
     pp.Suppress("(") - pp.Optional(arg_def_list, []) - pp.Suppress(")") -\
-    expr)#.set_parse_action(FuncExpr)
+    pp.Optional(pp.Suppress(":") - type_ident, None) -\
+    expr).set_parse_action(FuncExpr)
 
 
 statement = expr + pp.Suppress(";")
-block_expr = pp.Suppress("{") - pp.ZeroOrMore(statement) - pp.Optional(expr) - pp.Suppress("}")
+block_expr = (pp.Suppress("{") - pp.ZeroOrMore(statement) - pp.Optional(expr) - pp.Suppress("}")).set_parse_action(BlockExpr)
 expr << (func_expr | block_expr | math_expr | ident | literal)
 
-definition = pp.Keyword("const") - type_ident - pp.Suppress("=") - expr - pp.Optional(pp.Suppress(";"))
+definition = (pp.Keyword("const") - type_ident - pp.Suppress("=") - expr - pp.Optional(pp.Suppress(";"))).set_parse_action(Definition)
 
 program = pp.ZeroOrMore(definition)
-
-class CG:
-    def __init__(self):
-        self.globals = {}
-    def compile(self, parsed: list[Ast]):
-        pass
-        # for smt in parsed:
-            
 
 if __name__ == "__main__":
     def p(parser, text):
@@ -209,9 +343,13 @@ if __name__ == "__main__":
     # p(arg_def, "a: B::c<std::io::File>")
     # p(program, "const x = func() {}")
     # p(program, "const a::b = func(a: b) { 1; \"hello\"; 3.31; func() 2 };")
-    p(program, "const Vec<T>::new = func<T, U>(init_len: usize, allocator: std::alloc::Allocator<T>) { 1 }")
+    # p(program, "const Vec<T>::new = func<T, U>(init_len: usize, allocator: std::alloc::Allocator<T>) { 1 }")
+    p(expr, "1 + 1")
 
-    parsed = program.parse_string("const main = func() 0;")
+    parsed = program.parse_string("const main = func(argc: int, argv: **char): i32 { printf(\"Hello, world!\"); 0 }")
+    parsed.pprint()
     cg = CG()
-    print(cg.compile(parsed))
-
+    code = cg.compile(parsed)
+    print(code)
+    with open("out.c", "w") as f:
+        f.write(code)
